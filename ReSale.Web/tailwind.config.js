1386b35be0ac31cf7be407750f6d913ce8b6019b
Nallen <nooxie94@gmail.com>
2024-09-04 20:45:03 +0200

    Rewrite of the frontend

--- ReSale.Web/tailwind.config.js
+++ ReSale.Web/tailwind.config.js
@@ -13,16 +13,15 @@ module.exports = {
     },
     extend: {
       colors: {
-        'darker-gray': '#2b2b2b',
-        'dark-gray': '#3c3f41',
-        'table-gray': '#303030',
-        'light-gray': '#b4b9c0',
+        'background-primary': '#1c1c1c',
+        'background-secondary': '#2a2a2a',
         'green-primary': '#22a559',
         'green-secondary': '#17723d',
         'blue-primary': '#5765f2',
         'blue-secondary': '#444fbf',
         'red-primary': '#db5656',
         'red-secondary': '#b54747',
+        'parchment': '#F2E8C9'
       },
       height: {
         'screen-50': 'calc(100vh - 50px)',
